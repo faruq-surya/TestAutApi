34e7e8dbf96874800ddd562bee38955defdf83c6
Shaik Faruq <145796460+Faruq-1@users.noreply.github.com>
2026-07-22 15:09:00 +0530

    Add script to stop specified application pools

---
+++ scripts/StopPools.ps1
@@ -0,0 +1,25 @@
+Import-Module WebAdministration
+
+$pools = @(
+    "CARE_API",
+    "CARE_PL",
+    "CARE_WSL"
+)
+
+foreach ($pool in $pools) {
+
+    Write-Host "Stopping $pool..."
+
+    if ((Get-WebAppPoolState $pool).Value -eq "Started") {
+
+        Stop-WebAppPool $pool
+
+        Write-Host "$pool stopped."
+
+    }
+    else {
+
+        Write-Host "$pool already stopped."
+
+    }
+}
